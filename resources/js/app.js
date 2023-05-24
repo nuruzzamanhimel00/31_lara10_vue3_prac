bc3a86598ceca2fa621cde3ef58b5d2bc450467d
nuruzzamanHimel0 <nuruzzaman.himel147@gmail.com>
2023-05-24 14:57:30 +0600

    vue 3 setup into laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+
+const app = createApp({});
+
+import ExampleComponent from "./components/ExampleComponent.vue";
+app.component("example-component", ExampleComponent);
+
+// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
+//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
+// });
+
+app.mount("#app");
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Laravel 10 vue 3 setup test hello test again</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
